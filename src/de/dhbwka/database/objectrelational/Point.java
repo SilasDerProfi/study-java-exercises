4a944ec06b915a8265ed3e4ade201e8b62b188dc
SilasDerProfi <silas@schnurr.one>
2020-10-09 14:44:52 +0200

    Add Class Point

---
+++ src/de/dhbwka/database/objectrelational/Point.java
@@ -0,0 +1,57 @@
+package de.dhbwka.database.objectrelational;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="punkte")
+public class Point {
+    @Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+    private long id;
+
+    private double x;
+    private double y;
+    private double z;
+    
+    public Point(){
+        // default constructor for Mapping
+    }
+
+    public long getId() {
+		return this.id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public double getX() {
+		return this.x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return this.y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public double getZ() {
+		return this.z;
+	}
+
+	public void setZ(double z) {
+		this.z = z;
+	}
+
+
+    @Override
+    public String toString() {
+        return String.format("P(%d, %d, %d)", getX(), getY(), getZ());
+    }
+}
