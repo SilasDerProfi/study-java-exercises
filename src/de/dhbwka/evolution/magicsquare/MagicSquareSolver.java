dc8b7c08a9381971d69b259dc7a6e38a2b686622
SilasDerProfi <silas@schnurr.one>
2020-10-18 16:28:50 +0200

    Update MagicSquareSolver.java

--- src/de/dhbwka/evolution/magicsquare/MagicSquareSolver.java
+++ src/de/dhbwka/evolution/magicsquare/MagicSquareSolver.java
@@ -1,12 +1,30 @@
 package de.dhbwka.evolution.magicsquare;
 
-public class MagicSquareSolver {
+public final class MagicSquareSolver {
     
-    public void SolveStupidSwap(int maxChildCount){
-        // TODO: implement
+    private MagicSquareSolver(){
+        // Private constructor to avoid creating an instance of this "static" class
     }
 
-    public void SolveIntelligentSwap(int maxChildCount){
-        // TODO: implement
+    public static void SolveStupidSwap(MagicSquare unsolvedSquare, int maxChildCount){
+       // Swap cells with small value difference
+
+       // continue with the best child
+
+       // clear output
+
+       // print generation
+
+       // print best child
+
+    }
+
+    public static void SolveIntelligentSwap(MagicSquare unsolvedSquare, int maxChildCount){
+        // Calculate for all Cells the combined fitness (row + col fitness)
+        
+        // Swap cells where 
+        // | combinedFitness(a) − combinedFitness(b) | + (a.value − b.value)^2
+        // is low
+        
     }
 }
