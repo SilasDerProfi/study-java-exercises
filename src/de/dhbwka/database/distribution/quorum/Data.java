ca993d6ccbe0991aea6773f4637f3650036c114e
SilasDerProfi <silas@schnurr.one>
2020-11-02 12:57:37 +0100

    Add Quorum consensus implementation

---
+++ src/de/dhbwka/database/distribution/quorum/Data.java
@@ -0,0 +1,30 @@
+
+package de.dhbwka.database.distribution.quorum;
+
+public class Data {
+	private int mValue;
+	private int mVersion;
+
+	public Data() {
+		mValue = -1;
+		mVersion = -1;
+	}
+
+	public Data(int value, int version) {
+		mValue = value;
+		mVersion = version;
+	}
+
+	public int getValue() {
+		return mValue;
+	}
+
+	public int getVersion() {
+		return mVersion;
+	}
+
+	public void setValue(int value) {
+		mValue = value;
+		++mVersion;
+	}
+}
